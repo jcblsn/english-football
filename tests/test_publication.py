64b99d565576fa49bda6dd8351db780d3f5ece5e
Jacob Eliason <jacobeliason@gmail.com>
2026-09-15 09:30:03 -0400

    Change the public model version to v0.1

--- tests/test_publication.py
+++ tests/test_publication.py
@@ -131,7 +131,7 @@ def sample_run():
 
 
 def test_document_contracts_respect_the_boundary():
-    assert load_policy()["product"]["model_version"] == "v0.0"
+    assert load_policy()["product"]["model_version"] == "v0.1"
 
 
 def test_derived_forecast_drops_provider_evidence():
